fe61decfa1120e11f5bd5ebccfb9ed035b27a5e5
Lázaro Alves <lazaroalves@MacBook-Pro-de-Lazaro.local>
2022-09-07 19:44:32 -0300

    Implementing navigation between two screens

---
+++ screens/MealsOverviewScreen.js
@@ -0,0 +1,20 @@
+import { View, Text, StyleSheet } from 'react-native';
+
+import { MEALS} from '../data/dummy-data';
+
+function MealOverviewScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Meals Overview Screen</Text>
+    </View>
+  );
+};
+
+export default MealOverviewScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 16
+  }
+});
